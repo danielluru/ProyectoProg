66fc835b832a93e85f4831583058e891f1b8226e
Wild Republic Mini <37903095+WildRepublicMini@users.noreply.github.com>
2024-05-23 16:22:20 +0200

    Updating a few things

--- src/sqlEjercicio/agarrarDatos.java
+++ src/sqlEjercicio/agarrarDatos.java
@@ -4,6 +4,7 @@ package sqlEjercicio;
 import java.awt.Font;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
@@ -15,7 +16,13 @@ public class agarrarDatos extends  javax.swing.JFrame{
 	public agarrarDatos() {
 		JTextField textField=new JTextField("HOLA");
 		textField.setFont(new Font("Comic Sans MS", Font.PLAIN, 18));
+		JLabel label1 = new JLabel("Test");		
+		JLabel label2 = new JLabel("Test2");
+		JTextField textField2=new JTextField("HOLA2");
+		panel.add(label1);
 		panel.add(textField);
+		panel.add(label2);
+		panel.add(textField2);
 		f.add(panel);
 		f.setSize(350, 350);
 		f.setVisible(true);
--- src/sqlEjercicio/javaSwingParaSQL.java
+++ src/sqlEjercicio/javaSwingParaSQL.java
@@ -9,6 +9,7 @@ import java.awt.event.ActionListener;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
@@ -40,6 +41,17 @@ public class javaSwingParaSQL{
     		agarrarDatos ventana = new agarrarDatos();
     		}
     	});
+    	empleadoSupr.addActionListener((ActionListener) new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				agarrarDatos pitos = new agarrarDatos();
+				JTextField polla = new JTextField("Me mide 25 mm");
+				pitos.add(polla);
+				
+			}
+		});
     	salir.addActionListener((ActionListener) new ActionListener() {	// Add the action listener to the button
     		@Override
     		public void actionPerformed(ActionEvent e) {
